cf13238f4fd572782843cf1a9896eb8eacebcb29
Assaf <1214assaf@gmail.com>
2024-02-05 17:11:50 +0200

    setting default prop value and component types dynamically

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -27,7 +27,8 @@ export default function Examples() {
 
     return (
         <Section title="Examples" id="examples">
-        <Tabs buttons={ 
+        <Tabs 
+        buttons={ 
           <>         
           <TabButton 
           onClick={() => handleSelect('components')}
